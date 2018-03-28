12a2267a2bf33697de6cbee18e3e31bed49b44ed
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-03-28 02:18:42 -0700

    user validation specs and code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+  validates :username, :email, :encrypted_password, presence: true
+  validates :username, :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations" do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:encrypted_password) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:username) }
+  end
 end
