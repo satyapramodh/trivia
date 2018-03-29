9fe37f80f4f52197d0faaf1cf58387369160bcd8
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-03-29 16:16:10 -0700

    handle user email formats

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :username, :email, :encrypted_password, presence: true
   validates :username, :email, uniqueness: true
+
+  # email regex
+  # \A([^@;,`!#$%^&*(){}=\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z
+  validates_format_of :email, with: /\A([-+.\w]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,21 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:encrypted_password) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_uniqueness_of(:username) }
+    it { should allow_values("john.doe@example.com", "john+doe@example.com",
+    "john.doe1@example.com", "john-doe@example.com").
+          for(:email) }
+    it { should_not allow_values("john#doe@example.com", "john!doe@example.com",
+    "john@doe@example.com", "john=doe@example.com").
+          for(:email) }
   end
+
+  context "create" do
+    it "should create user with all params" do
+    end
+
+    it "should NOT create user with only some params" do
+    end
+
+  end
+
 end
