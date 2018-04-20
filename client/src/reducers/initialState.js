37c297afd62048eb5f1d06078439666b799768a7
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-04-19 20:16:10 -0700

    clean up node packages and update versions

--- client/src/reducers/initialState.js
+++
@@ -1,7 +0,0 @@
-export default {
-  questions: [],
-  scores: [],
-  currentUser: {},
-  notice: {},
-  error: {}
-};
--- client/src/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { fork } from "redux-saga/effects";
-import { watchRegisterUser } from "./watcher";
-
-// Here, we register our watcher saga(s) and export as a single generator
-// function (startForeman) as our root Saga.
-export default function* startForman() {
-  yield fork(watchRegisterUser);
-}
